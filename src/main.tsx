40d59888ee6203ecfcd7e0f44ea5c29bc76a28ed
guardiolaelec <gestionweb@gguardiola.com>
2026-07-07 23:58:08 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import './index.css';
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+// Añade esta línea para registrar la PWA automáticamente:
+import 'virtual:pwa-register';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
